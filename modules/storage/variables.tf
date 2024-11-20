442942609e51ba93810e843f7ffcc725bdf36b30
root <gabgebgib@gmail.com>
2024-11-21 00:43:57 +0800

    Update storage module variables

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,8 +1,18 @@
 variable "project" {}
-variable "env" {}
+variable "env" {
+  description = "The differentiator of the resource environment (e.g. dev, uat, prod)"
+  type = string
+  default = "dev"
+}
 
 variable "default_region" {
   description = "The region in which to create the google cloud storage bucket."
   type        = string
   default     = "europe-west1"
+}
+
+variable "bucket_name" {
+  description = "The name of the bucket you want to instantiate"
+  type = string
+  default = "data_lake_bucket"
 }
