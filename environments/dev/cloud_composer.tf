27cdb5b0e6f93a074075fed6b16ddfcde718fb46
root <gabgebgib@gmail.com>
2025-01-02 14:40:39 +0800

    Add Cloud Composer 2 resource, updated Composer service account roles

--- environments/dev/cloud_composer.tf
+++ environments/dev/cloud_composer.tf
@@ -1,12 +1,22 @@
+locals {
+  cloud_composer_roles = [
+    "roles/composer.worker",
+    "roles/bigquery.admin",
+    "roles/secretmanager.secretAccessor",
+    "roles/storage.admin"
+  ]
+}
+
 resource "google_service_account" "cloud_composer_environment_service_account" {
   account_id   = "${var.env}-cloud-composer-env-sa"
   display_name = "Cloud Composer environment service account for DEV"
 }
 
 resource "google_project_iam_member" "cloud_composer_environment_service_account" {
-  project = var.project
-  member  = "serviceAccount:${google_service_account.cloud_composer_environment_service_account.email}"
-  role    = "roles/composer.worker"
+  for_each = toset(local.cloud_composer_roles)
+  project  = var.project
+  member   = "serviceAccount:${google_service_account.cloud_composer_environment_service_account.email}"
+  role     = each.key
 }
 
 resource "google_project_iam_member" "cloud_composer_service_agent_service_account" {
@@ -15,24 +25,58 @@ resource "google_project_iam_member" "cloud_composer_service_agent_service_accou
   role    = "roles/composer.ServiceAgentV2Ext"
 }
 
-# TODO: Manage via Terraform Composer when Composer 3 images are supported 
-# Current keep getting this error message:
-# Error: "config.0.software_config.0.image_version" ("composer-3-airflow-2.10.2") doesn't match regexp "composer-([0-9]+\\.[0-9]+\\.[0-9]+(-preview\\.[0-9]+)?|latest)-airflow-([0-9]+\\.[0-9]+(\\.[0-9]+.*)?)"
-# The naming convention of composer 3 images appear to not be supported yet by the image_version parameter which is stupid. Using composer-3.0.0-airflow-2.10.2 also does not work because such image_version does not exist.
+resource "google_composer_environment" "cloud_composer_environment" {
+  name   = "${var.env}-${var.project}-cloud-composer-2-environment"
+  region = var.region
 
-# resource "google_composer_environment" "cloud_composer_environment" {
-#   name   = "${var.env}-${var.project}-cloud-composer-3-environment"
-#   region = var.region
+  config {
 
-#   config {
-#     software_config {
-#       image_version = "composer-3-airflow-2.10.2"
-#     }
+    software_config {
+      image_version = "composer-2.10.1-airflow-2.10.2"
 
-#     node_config {
-#       service_account = google_service_account.cloud_composer_environment_service_account.email
-#     }
-#   }
+      pypi_packages = {
+        google-api-python-client    = "==2.135.0"
+        google-cloud-storage        = "==2.18.2"
+        stripe                      = "==9.9.0"
+        google-cloud-bigquery       = "==3.27.0"
+        google-cloud-secret-manager = "==2.21.1"
+        pandas                      = "==2.2.3"
+      }
+    }
 
-#   depends_on = [ google_project_iam_member.cloud_composer_environment_service_account, google_project_iam_member.cloud_composer_service_agent_service_account ]
-# }
+    node_config {
+      service_account = google_service_account.cloud_composer_environment_service_account.email
+    }
+
+    workloads_config {
+      scheduler {
+        cpu        = 1
+        memory_gb  = 2
+        storage_gb = 5
+        count      = 1
+      }
+
+      triggerer {
+        count     = 1
+        cpu       = 0.5
+        memory_gb = 1
+      }
+
+      web_server {
+        cpu        = 1
+        memory_gb  = 2
+        storage_gb = 5
+      }
+
+      worker {
+        cpu        = 1.5
+        memory_gb  = 4
+        storage_gb = 10
+        min_count  = 2
+        max_count  = 6
+      }
+    }
+  }
+
+  depends_on = [google_project_iam_member.cloud_composer_environment_service_account, google_project_iam_member.cloud_composer_service_agent_service_account]
+}
