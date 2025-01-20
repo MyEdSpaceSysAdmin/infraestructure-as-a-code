dc02943a1e93d2b73729ff06b108d5f726b6bd84
root <gabgebgib@gmail.com>
2025-01-20 14:23:45 +0800

    Imported existing Datastream Connection Profile bigquery-sink to Terraform state, recreate existing Datastream for Bitrix chat data using Terraform, add Datastream Connection Profile and Stream for filament into Terraform

--- environments/dev/datastream.tf
+++ environments/dev/datastream.tf
@@ -4,8 +4,8 @@ data "google_secret_manager_secret_version" "bitrix_read_replica_password" {
 }
 
 resource "google_datastream_connection_profile" "bitrix_chat_mysql_connection" {
-  display_name              = "${var.env}-bitrix-chat-connection"
-  connection_profile_id     = "${var.env}-bitrix-chat-connection"
+  display_name              = "bitrix-chat"
+  connection_profile_id     = "bitrix-chat"
   location                  = var.region
   create_without_validation = false
 
@@ -13,25 +13,23 @@ resource "google_datastream_connection_profile" "bitrix_chat_mysql_connection" {
     hostname = "bitrix-read-replica.ctzlfpmjufbh.eu-west-2.rds.amazonaws.com"
     port     = 3306
     username = "admin"
-
     password = data.google_secret_manager_secret_version.bitrix_read_replica_password.secret_data
   }
 }
 
+# Imported from GCP sincere-hybrid-364510
 resource "google_datastream_connection_profile" "bigquery_sink_connection" {
-  display_name              = "${var.env}-bigquery-sink-connection"
-  connection_profile_id     = "${var.env}-bigquery-sink-connection"
+  display_name              = "bigquery-sink"
+  connection_profile_id     = "bigquery-sink"
   location                  = var.region
-  create_without_validation = false
 
   bigquery_profile {}
 }
 
-
 resource "google_datastream_stream" "bitrx_chat_mysql_to_bigquery" {
-    display_name = "${var.env}-eu-west-2-mysql-bitrix-read-replica-sitemanager-db-to-bq"
+    display_name = "eu-west-2-mysql-bitrix-read-replica-sitemanager-db-to-bq"
     location     = var.region
-    stream_id    = "${var.env}-eu-west-2-mysql-bitrix-read-replica-sitemanager-db-to-bq"
+    stream_id    = "eu-west-2-mysql-bitrix-read-replica-sitemanager-db-to-bq"
 
     source_config {
         source_connection_profile = google_datastream_connection_profile.bitrix_chat_mysql_connection.id
@@ -64,8 +62,71 @@ resource "google_datastream_stream" "bitrx_chat_mysql_to_bigquery" {
             source_hierarchy_datasets {
                 dataset_template {
                     location = var.region
-                    dataset_id_prefix = "${var.env}_bitrix_chat_"
+                    dataset_id_prefix = "bitrix_chat_"
                 }
+            }
+            merge {}
+        }
+    }
+
+    backfill_all {}
+    create_without_validation = false
+    desired_state = "RUNNING"
+}
+
+data "google_secret_manager_secret_version" "filament_read_replica_password" {
+  secret  = "filament-read-replica-password"
+  project = var.project
+}
+
+# Can't import existing Connection profile to Terraform config without triggering to recreate it
+# Just use this resource config when migrating to main projects
+resource "google_datastream_connection_profile" "filament_mysql_connection" {
+  display_name              = "${var.env}-filament-connection"
+  connection_profile_id     = "${var.env}-filament-connection"
+  location                  = var.region
+  create_without_validation = false
+
+  mysql_profile {
+    hostname = "filament-read-replica.ctzlfpmjufbh.eu-west-2.rds.amazonaws.com"
+    port     = 3306
+    username = "data_team"
+    password = data.google_secret_manager_secret_version.filament_read_replica_password.secret_data
+  }
+}
+
+resource "google_datastream_stream" "filament_mysql_to_bigquery" {
+    display_name = "${var.env}-eu-west2-mysql-filament-prod-db-to-bq"
+    location     = var.region
+    stream_id    = "${var.env}-eu-west2-mysql-filament-prod-db-to-bq"
+
+    source_config {
+        source_connection_profile = google_datastream_connection_profile.filament_mysql_connection.id
+        mysql_source_config {
+          include_objects {
+            mysql_databases  {
+              database = "filament_prod"
+            }
+            mysql_databases  {
+              database = "filament_staging"
+            }
+            mysql_databases  {
+              database = "filament_test"
+            }
+            mysql_databases  {
+              database = "vapor"
+            }
+          }
+          binary_log_position {}
+        }
+    }
+
+    destination_config {
+        destination_connection_profile = google_datastream_connection_profile.bigquery_sink_connection.id
+        bigquery_destination_config {
+            data_freshness = "900s"
+            single_target_dataset {
+              dataset_id = "${var.project}:filament"
             }
             merge {}
         }
